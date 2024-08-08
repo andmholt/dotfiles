c527c1420e277dee81c7964505d91090b594130f
Andrew Holt <amholt@usc.edu>
2024-08-08 10:38:13 -0700

    fix deps

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -4,7 +4,9 @@ return {
     "nvim-neotest/nvim-nio",
     "nvim-lua/plenary.nvim",
     "antoinemadec/FixCursorHold.nvim",
-    "nvim-treesitter/nvim-treesitter"
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-neotest/neotest-jest",
+    "marilari88/neotest-vitest",
   },
   config = function ()
     require('neotest').setup({
